6013982ba4d473485a677b33e4440bee89189a1e
Muhsin95952 <muhsinshah95952@gmail.com>
2025-07-19 11:55:32 +0500

    corrected the code

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import models, schemas, crud
+from . import schemas, crud
 from .database import engine, Base, get_db
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 @app.post("/posts/", response_model=schemas.Post)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return crud.create_post(db, post)
 
+
 @app.get("/posts/", response_model=list[schemas.Post])
 def read_posts(db: Session = Depends(get_db)):
     return crud.get_posts(db)
+
 
 @app.get("/posts/{post_id}", response_model=schemas.Post)
 def read_post(post_id: int, db: Session = Depends(get_db)):
@@ -21,6 +24,7 @@ def read_post(post_id: int, db: Session = Depends(get_db)):
     if not db_post:
         raise HTTPException(status_code=404, detail="Post not found")
     return db_post
+
 
 @app.delete("/posts/{post_id}")
 def delete_post(post_id: int, db: Session = Depends(get_db)):
